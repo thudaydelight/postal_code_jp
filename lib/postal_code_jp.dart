1885f9efd2d7a0d8eb2105164b015e8cda7507ec
sonvi0603 <sonvi0603@gmail.com>
2022-03-02 13:54:28 +0700

    commit 2
    commit 2

--- lib/postal_code_jp.dart
+++ lib/postal_code_jp.dart
@@ -15,10 +15,10 @@ class PostalCodeJp {
     if (postalCodeRegExp.hasMatch(postalCode)) {
       throw ArgumentError('The post code must be 7 character');
     }
-    print('FILE_PATH_CSV $latestPostalCodePath');
+
     final file =
         File("$latestPostalCodePath/${postalCode.substring(0, 3)}.csv");
-
+    print('FILE_PATH_CSV ${file.path}');
     if (!file.existsSync()) {
       return [];
     }
